6ad8496f8c08e50a031422240bfa1f9a89019eee
Kira Coder Gylling <kira.gylling@gmail.com>
2017-08-05 14:35:50 +0200

    recreated __init__ files

---
+++ kiramath/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from . import calc
+from . import comb
+from . import const
+from . import elem
+from . import linalg
+from . import markov
+from . import opt
+from . import rand
+from . import spec
+from . import stats
+from . import ts
